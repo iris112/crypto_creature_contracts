b1acc7a1351d95e291c4d3a01749dea76cd68ab8
iris112 <nglayi99@gmail.com>
2021-09-03 13:31:56 -0500

    working for contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,7 +26,13 @@ module.exports = {
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
-    }
+    },
+    binance_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: process.env.MNEMONIC}
+    },
   },
   contractSizer: {
     alphaSort: true,
